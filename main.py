564f5ba3be94f9e9ce3f9f1a87b15d8d8e13df0e
findley <findley.e.j@gmail.com>
2012-09-26 15:04:19 -0400

    player sprites move

--- main.py
+++ main.py
@@ -10,13 +10,23 @@ pygame.init()
 
 BG_COLOR = (0, 0, 0)
 PLAYER_COLORS = [(255, 0, 0), (0, 0, 255)]
-#PLAYER_SPRITES = [pygame.image.load(os.path.join('art', 'bla.png')),pygame.image.load(os.path.join('art', 'bla.png'))]
 SQ_SIZE = 50 # pixel size of each square
+#PLAYER_SPRITES = [pygame.image.load(os.path.join('art', 'bla.png')),pygame.image.load(os.path.join('art', 'bla.png'))]
+player_sprite = pygame.image.load("art/royalghost.png")
+p1_right = pygame.transform.scale(player_sprite.subsurface(7,21,160,190),(SQ_SIZE-10,SQ_SIZE-10))
+p1_front = pygame.transform.scale(player_sprite.subsurface(7,261,175,190),(SQ_SIZE-10,SQ_SIZE-10))
+p1_left = pygame.transform.scale(player_sprite.subsurface(471,21,160,190),(SQ_SIZE-10,SQ_SIZE-10))
+p1_back = pygame.transform.scale(player_sprite.subsurface(519,261,175,190),(SQ_SIZE-10,SQ_SIZE-10))
+p2_right = pygame.transform.scale(player_sprite.subsurface(231,21,160,190),(SQ_SIZE-10,SQ_SIZE-10))
+p2_front = pygame.transform.scale(player_sprite.subsurface(264,261,175,190),(SQ_SIZE-10,SQ_SIZE-10))
+p2_left = pygame.transform.scale(player_sprite.subsurface(695,21,160,190),(SQ_SIZE-10,SQ_SIZE-10))
+p2_back = pygame.transform.scale(player_sprite.subsurface(759,261,175,190),(SQ_SIZE-10,SQ_SIZE-10))
+PLAYER_SPRITES=[[p1_front,p1_back,p1_left,p1_right],[p2_front,p2_back,p2_left,p2_right]]
 WALL_HEIGHT = 10
 WALL_WIDTH = SQ_SIZE + WALL_HEIGHT
 MAZE = maze_from_file("bigmaze.txt")
 SURFACE = pygame.display.set_mode((SQ_SIZE * MAZE.width()+SQ_SIZE/10, SQ_SIZE * MAZE.height()+SQ_SIZE/10))
-PLAYERS = [Player([x for x in MAZE.starting_locations[i]], PLAYER_COLORS[i])
+PLAYERS = [Player([x for x in MAZE.starting_locations[i]], i, "FRONT")
 					 for i in range(len(MAZE.starting_locations))]
 
 FONT = pygame.font.SysFont(None, 48)
@@ -73,8 +83,19 @@ while True:
 		for player in PLAYERS:
 				position = player.position
 				# IMPORTANT: MAZE and pygame use reversed coordinates, so we have to flip here.
-				screen_position = (int(SQ_SIZE * (position[1] + 0.5)), int(SQ_SIZE * (position[0] + 0.5)))
-				pygame.draw.circle(SURFACE, player.color, screen_position, int(SQ_SIZE * 0.3))
+				screen_position = (int(SQ_SIZE * (position[1] + 0.2)), int(SQ_SIZE * (position[0] + 0.2)))
+				#pygame.draw.circle(SURFACE, player.color, screen_position, int(SQ_SIZE * 0.3))
+				if player.direction=="FRONT":
+					index = 0
+				elif player.direction=="BACK":
+					index = 1
+				elif player.direction=="LEFT":
+					index = 2
+				elif player.direction=="RIGHT":
+					index = 3
+				else:
+					index=0
+				SURFACE.blit(PLAYER_SPRITES[player.number][index],screen_position)
 
 		pygame.display.update()
                 
--- player.py
+++ player.py
@@ -3,15 +3,19 @@ import pygame
 class Player:
     clock = pygame.time.Clock()
     position = [0, 0]
-    color = (255, 255, 255)
+    #color = (255, 255, 255)
+    direction = "FRONT"
+    number = 0
     turn_time = 300
     time_till_move = turn_time
     power_points = 0
     
-    def __init__(self, position, color):
+    def __init__(self, position, number, direction):
         self.position = position
-        self.color = color
+        self.number = number
+        self.direction = direction
     def up(self, maze):
+        self.direction = "BACK"
         print self.time_till_move - self.clock.get_time()
         if not maze.horiz_walls[self.position[0]][self.position[1]] and self.clock.get_time()>self.time_till_move:
             self.position[0] = self.position[0] - 1
@@ -20,6 +24,7 @@ class Player:
             self.time_till_move = self.time_till_move - self.clock.get_time()
         self.clock.tick()
     def down(self, maze):
+        self.direction = "FRONT"
         print self.time_till_move - self.clock.get_time()
         if not maze.horiz_walls[self.position[0]+1][self.position[1]] and self.clock.get_time()>self.time_till_move:
             self.position[0] = self.position[0] + 1
@@ -28,6 +33,7 @@ class Player:
             self.time_till_move = self.time_till_move - self.clock.get_time()
         self.clock.tick()   
     def left(self, maze):
+        self.direction = "LEFT"
         print self.time_till_move - self.clock.get_time()
         if not maze.vert_walls[self.position[0]][self.position[1]] and self.clock.get_time()>self.time_till_move:
             self.position[1] = self.position[1] - 1
@@ -36,6 +42,7 @@ class Player:
             self.time_till_move = self.time_till_move - self.clock.get_time()
         self.clock.tick()
     def right(self, maze):
+        self.direction = "RIGHT"
         print self.time_till_move - self.clock.get_time()
         if not maze.vert_walls[self.position[0]][self.position[1]+1] and self.clock.get_time()>self.time_till_move:
             self.position[1] = self.position[1] + 1
